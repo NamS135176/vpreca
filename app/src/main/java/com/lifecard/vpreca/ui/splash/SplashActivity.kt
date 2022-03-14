70e541517929f7fbff7e40c107dcb2c89ecf3f25
namnt <namnt@vn-sis.com>
2022-03-14 07:56:48 +0700

    policy screen

--- app/src/main/java/com/lifecard/vpreca/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/lifecard/vpreca/ui/splash/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import com.lifecard.vpreca.LoginActivity
 import com.lifecard.vpreca.MainActivity
 import com.lifecard.vpreca.R
+import com.lifecard.vpreca.SignupActivity
 import com.lifecard.vpreca.data.UserRepository
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
@@ -21,11 +22,12 @@ class SplashActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_splash)
 
         println("SplashActivity... onCreate: ${userRepository.user}")
-        if (userRepository.isLoggedIn) {
-            navigateToLoginScreen()
-        } else {
-            navigateToMainScreen()
-        }
+//        if (userRepository.isLoggedIn) {
+//            navigateToLoginScreen()
+//        } else {
+//            navigateToMainScreen()
+//        }
+        navigateToSignUpScreen()
     }
 
     private fun navigateToMainScreen() {
@@ -37,6 +39,13 @@ class SplashActivity : AppCompatActivity() {
 
     private fun navigateToLoginScreen() {
         val intent = Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        startActivity(intent)
+    }
+
+    private fun navigateToSignUpScreen() {
+        val intent = Intent(this, SignupActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         startActivity(intent)
