4bbcd63028449d85b5c7d1c6abcb4a44bba481a6
anhndt <anhndt@macbook.pro>
2022-06-09 14:02:59 +0700

    fix bug LC_VPC-171

--- app/src/test/java/com/lifecard/vpreca/utils/ConverterTest.kt
+++ app/src/test/java/com/lifecard/vpreca/utils/ConverterTest.kt
@@ -7,16 +7,16 @@ import java.util.*
 
 class ConverterTest {
     @Test
-    fun convertDateTimeMonth_isCorrect() {
+    fun formatCardDateTimeMonth_isCorrect() {
         val strDate1 = "2022-05-27T10:00:00-0700"
         val strDate2 = "2022-12-27T10:00:00-0700"
         val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm")
         val date1: Date = dateFormat.parse(strDate1) as Date
         val date2: Date = dateFormat.parse(strDate2) as Date
 
-        Assert.assertEquals("2022月5日", Converter.convertDateTimeMonth(date1))
-        Assert.assertEquals("2022月12日", Converter.convertDateTimeMonth(date2))
-        Assert.assertEquals("", Converter.convertDateTimeMonth(null))
+        Assert.assertEquals("20225月27日", Converter.formatCardDateTimeMonth(date1))
+        Assert.assertEquals("202212月27日", Converter.formatCardDateTimeMonth(date2))
+        Assert.assertEquals("", Converter.formatCardDateTimeMonth(null))
     }
 
     @Test
